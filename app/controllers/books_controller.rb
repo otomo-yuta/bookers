1868174b2e1aa2312303027baf77507041752f49
otomo-yuta <120460339+otomo-yuta@users.noreply.github.com>
2022-12-26 13:47:58 +0900

    [Finish]bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  # 一覧ページの機能
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] ="Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+  # 詳細ページの機能
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  # 編集ページの機能
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
